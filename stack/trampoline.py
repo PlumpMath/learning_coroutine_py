7f8fcac7ec5fee22515d7691cb98d262c55c3b25
roy <roy_radio@126.com>
2015-02-06 15:01:34 +0800

    more examples

---
+++ stack/trampoline.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# A simple of example of trampoling between coroutines
+
+# A subroutine
+def add(x, y):
+    yield x + y
+
+# A function that calls a subroutine
+def main():
+    r = yield add(2, 2)
+    print r
+    yield
+
+def run():
+    m = main()
+    # An example of a "trampoline"
+    sub = m.send(None)
+    result = sub.send(None)
+    m.send(result)
+
+run()
